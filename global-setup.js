091770e3d4a1b6545d0b2c61d4534d1344382b90
Ekaterina <luneveks@gmail.com>
2022-05-02 13:48:56 +0300

    Update global-setup.js

--- global-setup.js
+++ global-setup.js
@@ -1,21 +1,18 @@
 const { chromium, firefox, webkit } = require("@playwright/test");
 
-module.exports = async config => {
+module.exports = async (config) => {
   const browser = await chromium.launch();
 
   const page = await browser.newPage();
 
   // Authorizing with test user
   // This step will provide completion of the precondition in every case: Logged user on /projects page
-  
-  await page.goto("https://app.stage.lokalise.cloud/", {timeout:120000});
+
+  await page.goto("https://app.stage.lokalise.cloud/", { timeout: 120000 });
 
   await page.waitForTimeout(8000);
 
-  await page.fill(
-    'input[placeholder="user@company.com"]',
-    "login"
-  );
+  await page.fill('input[placeholder="user@company.com"]', "login");
 
   await page.fill('input[placeholder="password"]', "password");
 
